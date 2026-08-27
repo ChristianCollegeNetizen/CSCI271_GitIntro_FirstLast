065b75aa4cb0a8c5ad16fa7c6670b97d128b0c45
Christian <321620592+ChristianCollegeNetizen@users.noreply.github.com>
2026-08-26 20:01:01 -0700

    Update header info

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,6 @@
 // CSCI 27X - GitHub Upload Practice
-// Name: YOUR NAME
-// Section: YOUR SECTION
+// Name: Christian Calero Tovar
+// Section: MJC GitHub Assignment
 
 #include <iostream>
 using namespace std;
